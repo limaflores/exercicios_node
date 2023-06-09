a4dbf939fa0ff9fcdfbd0f94a210d660c011e95d
Felipe <limaflores@gmail.com>
2023-06-09 00:16:22 -0300

    Test AI improvements

--- 1_INTRO/2_utilizando_modulo/index.js
+++ 1_INTRO/2_utilizando_modulo/index.js
@@ -1,12 +1,12 @@
-
-const fs = require("fs") // file system
-
+// Import the file system module
+const fs = require("fs");
+ // Read the file 'arquivo.txt' in UTF-8 format
 fs.readFile('arquivo.txt', 'utf8', (err, data) => {
+    // If there's an error while reading the file, log the error and stop the function
     if (err) {
-        console.log(err)
-        return
+        console.log(err);
+        return;
     }
-
-    console.log(data)
-    }
-)
+     // If the file is successfully read, log the file data
+    console.log(data);
+});
