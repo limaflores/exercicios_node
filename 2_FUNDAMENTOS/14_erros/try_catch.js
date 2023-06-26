560512cd67379a20211f3cbb7173d3810db0b705
Felipe <limaflores@gmail.com>
2023-06-25 23:12:04 -0300

    Add improvements

--- 2_FUNDAMENTOS/14_erros/try_catch.js
+++ 2_FUNDAMENTOS/14_erros/try_catch.js
@@ -1,9 +1,9 @@
 
 const x = 10
 
-// checar se x é um número
+// vai tentar atribuir um novo número a uma constante(o que não pode)
 try {
-    x =2 
+    x = 2 
 } catch (err) {
     console.log(`Erro: ${err}`)
 }
