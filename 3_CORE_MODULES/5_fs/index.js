d3a918c459aa2490eccb400b90d7ea20b7359e56
Felipe <limaflores@gmail.com>
2023-06-28 23:28:43 -0300

    Improve clean code

--- 3_CORE_MODULES/5_fs/index.js
+++ 3_CORE_MODULES/5_fs/index.js
@@ -1,17 +1,13 @@
-const { fstat } = require("fs")
 const http = require("http")
 const fs = require('fs')
-
-const port = 3000
-
-const server = http.createServer((req, res) => {
+ const port = 3000
+ const server = http.createServer((req, res) => {
     fs.readFile('mensagem.html', function (err, data) {
         res.writeHead(200, { 'Content-Tupe': 'text/html'})
         res.write(data)
         return res.end()
     })
 })
-
-server.listen(port, () => {
+ server.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`)
 })
