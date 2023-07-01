832a5e90604808dc17d21960ad8f4eff58f34208
Felipe <limaflores@gmail.com>
2023-06-30 23:40:47 -0300

    Improve clean code

--- 3_CORE_MODULES/6_escrevendo_arquivos/index.js
+++ 3_CORE_MODULES/6_escrevendo_arquivos/index.js
@@ -1,7 +1,6 @@
 const http = require('http')
 const fs = require('fs')
 const port = 3000
-
 const server = http.createServer((req, res) => {
     const urlInfo = require('url').parse(req.url, true)
     const name = urlInfo.query.name
