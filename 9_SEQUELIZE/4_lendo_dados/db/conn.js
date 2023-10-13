26c2f48ddb7f09baba3aed9c0d8fe192d4d56d86
Felipe <limaflores@gmail.com>
2023-10-12 21:25:19 -0300

    Add class 9-4 read files

---
+++ 9_SEQUELIZE/4_lendo_dados/db/conn.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('nodesequelize2', 'meta', 'root', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3307,    
+})
+
+// try {
+//     sequelize.authenticate();
+//     console.log('Conextamos com sucesso o Sequelize');  
+// } catch (error) {
+//     console.log('Erro ao estabelecer conexão com o banco de dados:', error);    
+// }
+
+module.exports = sequelize
