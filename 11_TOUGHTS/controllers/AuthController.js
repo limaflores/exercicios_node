8f516acdf3796671bcd48c51816c3e3067d7074b
Felipe <limaflores@gmail.com>
2024-01-01 22:29:43 -0300

    Add auth controller class 11

---
+++ 11_TOUGHTS/controllers/AuthController.js
@@ -0,0 +1,92 @@
+const { runInNewContext } = require('vm');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+module.exports = class AuthController{
+    static login(req, res) {
+        res.render('auth/login');
+    }
+
+    static async loginPost(req, res) {
+        const { email, password } = req.body;
+
+        // Find user
+        const user = await User.findOne({ where: { email: email }});
+        if (!user) {
+            req.flash('message', 'O email informado não está registrado!');            
+            res.render('auth/login');
+            return
+        }
+
+        // Check if password match
+        const passwordMatch = bcrypt.compareSync(password, user.password);
+        if (!passwordMatch) {
+            req.flash('message', 'A senha informada não é valida!');            
+            res.render('auth/login');
+            return
+        }
+
+        // Initialize session
+        req.session.userid = user.id
+        req.flash('message', 'Login efetuado com sucesso!');
+        req.session.save(() => {
+            res.redirect('/');
+        })
+        
+    }
+
+    static register(req, res) {
+        res.render('auth/register');
+    }
+
+    static async registerPost(req, res) {
+        const { name, email, password,confirmpassword } = req.body;
+        
+        // Password Validation
+        if (password != confirmpassword) {
+            req.flash('message', 'As senhas não conferem, tente novamente!');            
+            res.render('auth/register');
+            return 
+        }
+
+        // Check if user exists
+        const  checkIfUserExists = await User.findOne({ where: { email: email }});
+        if (checkIfUserExists) {
+            req.flash('message', 'O email informado já existe, tente outro!');            
+            res.render('auth/register');
+            return
+        }
+
+        // Create a password hash
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try {
+            const createdUser = await User.create(user)
+
+            // Initialize session
+            req.session.userid = createdUser.id
+
+            req.flash('message', 'Conta criada com sucesso!');
+
+            req.session.save(() => {
+                res.redirect('/');
+            })
+            
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    static logout(req, res) {
+        req.session.destroy();
+        res.redirect('/login');
+    }
+}
